228db2abfb437cf1b6249a367c3301e261751602
Jeffry Molanus <jeffry.molanus@gmail.com>
2020-06-26 10:23:41 +0200

    use rust-toolchain

--- nix/lib/rust.nix
+++ nix/lib/rust.nix
@@ -3,12 +3,6 @@ let
   pkgs = import sources.nixpkgs { overlays = [ (import sources.nixpkgs-mozilla) ]; };
 in
 rec {
-  nightly = pkgs.rustChannelOf {
-    channel = "nightly";
-    data = "2020-06-26";
-  };
-
-  stable = pkgs.rustChannelOf {
-    channel = "stable";
-  };
+  nightly = pkgs.rustChannelOf { rustToolchain = ../.././rust-toolchain; };
+  stable = pkgs.rustChannelOf { channel = "stable"; };
 }
