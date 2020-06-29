ad0ca650d362cf98516bb63884c4888cc3ad9bdf
Jeffry Molanus <jeffry.molanus@gmail.com>
2020-06-29 14:19:05 +0200

    add csi

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,7 @@ cargo build --all
 ( cd jsonrpc && cargo test )
 ( cd mayastor && cargo test -- --test-threads=1 )
 ( cd mayastor-test && npm install && ./node_modules/mocha/bin/mocha test_cli.js )
+( cd mayastor-test && ./node_modules/mocha/bin/mocha test_csi.js )
 ( cd mayastor-test && ./node_modules/mocha/bin/mocha test_replica.js )
 ( cd mayastor-test && ./node_modules/mocha/bin/mocha test_nexus.js )
 ( cd mayastor-test && ./node_modules/mocha/bin/mocha test_rebuild.js )
